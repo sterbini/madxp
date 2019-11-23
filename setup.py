f9591087bf44cc1e0506d3a5bbc9e313394952a6
sterbini <guido.sterbini@cern.ch>
2019-11-23 17:40:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='madxp',
+      version='0.01',
+      description='TODO',
+      url='https://github.com/sterbini/madxp',
+      author='Guido STERBINI',
+      author_email='guido.sterbini@cern.ch',
+      license='MIT',
+      packages=['madx'],
+      zip_safe=False)
