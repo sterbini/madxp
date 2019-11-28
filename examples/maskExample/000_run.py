746f9c19c8e3606a042f1822b4777f6d2de07c2a
sterbini <guido.sterbini@cern.ch>
2019-11-28 15:50:39 +0100

    Bug in cleaning

---
+++ examples/maskExample/000_run.py
@@ -0,0 +1,11 @@
+# %% Run it
+import madxp; 
+madxp.madxp('mask.madx', verbose=True)
+
+# %% Read the output.pkl
+import pandas as pd
+myDF=pd.read_pickle('output.pkl')
+myDF
+
+# %% Make markdown
+madxp.madx2md('input.madx','input.md')
