6e0c683310c646522deb2dfe06314173bf27353b
sterbini <guido.sterbini@cern.ch>
2019-11-28 15:10:33 +0100

    Make MD

--- examples/simpleFodo/000_run.py
+++ examples/simpleFodo/000_run.py
@@ -1,8 +1,12 @@
-# %%
+# %% Run it
 import madxp; 
 madxp.madxp('input.madx', verbose=True)
 
-# %%
-! pwd
+# %% Read the output.pkl
+import pandas as pd
+myDF=pd.read_pickle('output.pkl')
+myDF
 
-# %%
+# %% Make markdown
+madxp.madx2md('input.madx','input.md')
+
